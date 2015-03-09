d512cc644ee165470e1e4421c285e497d920ec90
Conor Smyth <cnrsmyth@gmail.com>
2015-03-09 16:14:41 -0700

    need to fix transition to and from create listing

--- LostFound/LFProject/LFProfileViewController.m
+++ LostFound/LFProject/LFProfileViewController.m
@@ -107,14 +107,12 @@
     [self performSegueWithIdentifier:@"logOut" sender:self];
 }
 
-/*
 #pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
+//In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
 }
-*/
 
 @end
