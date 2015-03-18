f174b404e78fc44003c1c00a0a8783c90a20463d
Conor Smyth <cnrsmyth@gmail.com>
2015-03-18 13:27:54 -0700

    Added comments and some formatting

--- LostFound/LFCategoriesViewController.h
+++ LostFound/LFCategoriesViewController.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "Listing.h"
 
 @interface LFCategoriesViewController : UITableViewController
 
--- LostFound/LFCategoriesViewController.m
+++ LostFound/LFCategoriesViewController.m
@@ -8,7 +8,6 @@
 
 #import "LFCategoriesViewController.h"
 #import "LFCreateListingViewController.h"
-#import "Listing.h"
 
 @interface LFCategoriesViewController ()
 
@@ -20,12 +19,6 @@ static NSString *cat;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -35,26 +28,26 @@ static NSString *cat;
 
 #pragma mark - Table view data source
 
+/* Display table functions */
+
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    // Return the number of sections.
+    /* Return the number of sections. */
     return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    // Return the number of rows in the section.
+    /* Return the number of rows in the section. */
     return 7;
 }
 
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
     cell.tag = indexPath.row;
-    switch (cell.tag) {
+    
+    switch(cell.tag) {
         case 0:
             cat = @"animals";
             self.category = cat;
-            //NSLog(self.category);
-            //[self performSegueWithIdentifier:@"sendData" sender:self];
             break;
         case 1:
             cat = @"clothing";
@@ -65,45 +58,44 @@ static NSString *cat;
             self.category = cat;
             break;
         case 3:
-            cat = (@"tickets");
+            cat = @"tickets";
             self.category = cat;
             break;
         case 4:
-            cat = (@"other");
+            cat = @"other";
             self.category = cat;
             break;
         case 5:
-            cat = (@"electronics");
+            cat = @"electronics";
             self.category = cat;
             break;
         case 6:
-            cat = (@"personal");
+            cat = @"personal";
             self.category = cat;
             break;
         default:
             break;
     }
 }
+
+/* Functions to send data over segue to other view controller */
+
 - (IBAction)sendData:(id)sender {
-    NSLog(cat);
     self.category = cat;
+    
     [self performSegueWithIdentifier:@"sendData" sender:self];
 }
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    if ([segue.destinationViewController isKindOfClass:[LFCreateListingViewController class]]) {
-            LFCreateListingViewController *mainViewConroller = segue.destinationViewController;
-            if (self.category) {
-                NSLog(self.category);
-                mainViewConroller.category = self.category;
-            }
-            else{
-                mainViewConroller.category = @"";
-                NSLog(self.category);
-            }
-    }
-    
-}
 
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    if ([segue.destinationViewController isKindOfClass:[LFCreateListingViewController class]]) {
+        LFCreateListingViewController *mainViewConroller = segue.destinationViewController;
+        
+        if(self.category) {
+            mainViewConroller.category = self.category;
+        } else {
+            mainViewConroller.category = @"";
+        }
+    }
+}
 
 @end
